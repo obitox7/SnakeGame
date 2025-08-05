187afccf51c3696a93a293bc17eb43c6447bfbce
Keshav sharma <135090664+obitox7@users.noreply.github.com>
2025-08-05 12:16:56 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -3,91 +3,65 @@ let screen = document.getElementById("screen");
 let head = document.getElementById("head");
 let w = screen.offsetWidth;
 let h = screen.offsetHeight;
-let y = parseInt(head.style.top) || 0;
-let x = parseInt(head.style.left) || 0;
+let y = parseInt(head.style.top) || 10;
+let x = parseInt(head.style.left) || 10;
 let count = 2;
-let idx = 1;
-let path = [];
+let ix = 0;
+let iy = 0;
+let pathx = [];
+let pathy = [];
 let bodies = [];
 let foodx = "";
 let foody = "";
 let food = "";
+let lastframe = 0;
+speed = 5;
+let dir = "right";
+genfood();
+
+function run(ctime) {
+    window.requestAnimationFrame(run);
+    if ((ctime - lastframe) / 1000 < 1 / speed) {
+        return;
+    }
+    if (dir == "up" && y > 0) {
+        y--;
+    } else if (dir == "down" && y < h) {
+        y++;
+    } else if (dir == "left" && x > 0) {
+        x--;
+    } else if (dir == "right" && x < w) {
+        x++;
+    }
+    position();
+}
+window.requestAnimationFrame(run);
+function up() {
+    if (dir !== "down") dir = "up";
+}
+function down() {
+    if (dir !== "up") dir = "down";
+}
+function left() {
+    if (dir !== "right") dir = "left";
+}
+function right() {
+    if (dir !== "left") dir = "right";
+}
 
 function position() {
     head.style.top = `${y}px`;
     head.style.left = `${x}px`;
-    path.push({ x, y });
-
-    if (path.length > count) {
-        path.splice(0, path.length - count);
+    pathx.push(x);
+    if (pathx.length > count) {
+        pathx.splice(0, pathx.length - count);
     }
-    
+    pathy.push(y);
+    if (pathy.length > count) {
+        pathy.splice(0, pathy.length - count);
+    }
+    move();
     colide();
-}
-
-let first = "";
-let second = "";
-let third = "";
-let fourth = "";
-right();
-genfood();
-function up() {
-    clearInterval(second);
-    clearInterval(third);
-    clearInterval(fourth);
-    clearInterval(first);
-
-    first = setInterval(() => {
-        if (y > 0) {
-            y--;
-            position();
-        } else {
-            clearInterval(first);
-        }
-    }, 20);
-}
-function down() {
-    clearInterval(first);
-    clearInterval(third);
-    clearInterval(fourth);
-    clearInterval(second);
-    second = setInterval(() => {
-        if (y !== h - 20) {
-            y++;
-            position();
-        } else {
-            clearInterval(second);
-        }
-    }, 20);
-}
-function left() {
-    clearInterval(second);
-    clearInterval(first);
-    clearInterval(fourth);
-    clearInterval(third);
-    third = setInterval(() => {
-        if (x > 0) {
-            x--;
-            position();
-        } else {
-            clearInterval(third);
-        }
-    }, 20);
-}
-function right() {
-    clearInterval(second);
-    clearInterval(third);
-    clearInterval(first);
-    clearInterval(fourth);
-
-    fourth = setInterval(() => {
-        if (x !== w - 20) {
-            x++;
-            position();
-        } else {
-            clearInterval(fourth);
-        }
-    }, 20);
 }
 function genfood() {
     let r1 = w - 20;
@@ -97,20 +71,57 @@ function genfood() {
     food = document.createElement("div");
     screen.appendChild(food);
     food.classList.add("food");
-    food.style.top = `${foody}px`;
-    food.style.left = `${foodx}px`;
+    food.style.transform = `translate(${foodx}px,${foody}px)`;
 }
 function eat() {
     screen.removeChild(food);
     genfood();
     grow();
+    grow();
+    grow();
+    grow();
+    grow();
+    grow();
+    grow();
+    grow();
+    grow();
+    grow();
+    grow();
+    grow();
+    grow();
+    grow();
+    grow();
+    grow();
+    grow();
+    grow();
+    grow();
+    grow();
 }
 function colide() {
-    let diffx = Math.abs(foodx - path[0].x);
-    let diffy = Math.abs(foody - path[0].y);
-    if (diffx < 20 && diffy < 20) {
+    const headSize = 15;
+    const foodSize = 15;
+
+    const headLeft = x;
+    const headRight = x + headSize;
+    const headTop = y;
+    const headBottom = y + headSize;
+
+    const foodLeft = foodx;
+    const foodRight = foodx + foodSize;
+    const foodTop = foody;
+    const foodBottom = foody + foodSize;
+
+    const isOverlap =
+        headLeft < foodRight &&
+        headRight > foodLeft &&
+        headTop < foodBottom &&
+        headBottom > foodTop;
+
+    if (isOverlap) {
         console.log("eat");
         eat();
+        iy += 1;
+        ix += 1;
     }
 }
 function grow() {
@@ -118,8 +129,14 @@ function grow() {
     let body = document.createElement("div");
     screen.appendChild(body);
     body.classList.add("body");
-    bodies.push("body");
+    bodies.push(body);
 }
-console.log(path[1].x);
-console.log(idx, count);
-console.log("end");
+function move() {
+    for (let i = 0; i < bodies.length; i++) {
+        let px = pathx[pathx.length - (i + 1)];
+        let py = pathy[pathy.length - (i + 1)];
+        if (px !== undefined && py !== undefined) {
+            bodies[i].style.transform = `translate(${px}px,${py}px)`;
+        }
+    }
+}
